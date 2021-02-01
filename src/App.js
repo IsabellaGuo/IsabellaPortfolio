f7a74c463f2464b1ceb05e9cfad1d98dcf29bb35
IsabellaGuo <isabella.guofei@gmail.com>
2021-02-01 14:58:56 -0800

    Project setup.

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import Header from "./components/Header.js";
+import "./components/Header.css";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Projects from "./components/Projects.js";
+import Footer from "./components/Footer.js";
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/about">
+            <Header />
+            <About />
+            <Footer />
+          </Route>
+          <Route path="/contact">
+            <Header />
+            <Contact />
+            <Footer />
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <App />
+            <Footer />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

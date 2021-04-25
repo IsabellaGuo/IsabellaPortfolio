e8f9450c5a21c7a0e21b07d2412f5da05ba494b8
IsabellaGuo <isabella.guofei@gmail.com>
2021-04-25 16:00:47 -0700

    Built contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,159 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import * as yup from "yup";
+import * as emailjs from "emailjs-com";
+import axios from "axios";
+
 import "./Contact.css";
 
 function Contact() {
-    return (
-        <div className="contact__main">
-            <p>If you have a project in mind</p>
-            <button>Contact Me</button>
-        </div>
-    )
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+  const [serverError, setServerError] = useState("");
+
+  const [buttonIsDisabled, setButtonIsDisabled] = useState(true);
+
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const [post, setPost] = useState([]);
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    //postData(formState)
+    emailjs
+      .sendForm("gmail", "isabella", ".form", "user_btTiErros7eOOIrRBXfmn")
+      .then(
+        result => {
+          console.log(result.text);
+        },
+        error => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  //inline validation
+  const validateChange = e => {
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(
+        e.target.type === "checkbox" ? e.target.checked : e.target.value
+      )
+      .then(valid => {
+        setErrors({ ...errors, [e.target.name]: "" });
+      })
+      .catch(err => {
+        setErrors({ ...errors, [e.target.name]: err.errors[0] });
+      });
+  };
+
+  const formSubmit = e => {
+    e.preventDefault();
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then(resp => {
+        setPost(resp.data);
+        setServerError(null);
+        setFormState({
+          name: "",
+          email: "",
+          message: ""
+        });
+      })
+      .catch(err => {
+        setServerError("something went wrong!");
+      });
+  };
+
+  const inputChange = e => {
+    e.persist();
+    const newFormState = {
+      ...formState,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value
+    };
+
+    validateChange(e);
+    setFormState(newFormState);
+  };
+
+  const formSchema = yup.object().shape({
+    name: yup.string().required("Name is required."),
+
+    email: yup.string().email(),
+
+    message: yup.string().required("Please let me know what I can do for you.")
+  });
+
+  useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      setButtonIsDisabled(!valid);
+    });
+  }, [formState]);
+  return (
+    <div className="contact__main">
+      <p>If you have a project in mind</p>
+      <form className="form" onSubmit={onFormSubmit}>
+        {serverError && <p className="error">{serverError}</p>}
+        <label className="label__text" htmlFor="name">
+          Name
+          <input
+            placeholder="Name"
+            id="name"
+            type="text"
+            name="name"
+            value={formState.name}
+            onChange={inputChange}
+          />
+          {errors.name.length > 0 ? (
+            <p className="error">{errors.name}</p>
+          ) : null}
+        </label>
+
+        <label className="label__text" htmlFor="email">
+          Email
+          <input
+            placeholder="Email"
+            id="email"
+            type="text"
+            name="email"
+            value={formState.email}
+            onChange={inputChange}
+          />
+          {errors.email.length > 0 ? (
+            <p className="error">{errors.email}</p>
+          ) : null}
+        </label>
+        <label className="label__text" htmlFor="message">
+          Message
+          <textarea
+            placeholder="Message"
+            id="message"
+            name="message"
+            value={formState.message}
+            onChange={inputChange}
+          />
+          {errors.message.length > 0 ? (
+            <p className="error">{errors.message}</p>
+          ) : null}
+        </label>
+        <button
+          className="contact__button"
+          type="submit"
+          disabled={buttonIsDisabled}
+        >
+          Contact Me
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,8 +9,15 @@ import Contact from "./Contact.js";
 import "./Contact.css";
 import Footer from "./Footer.js";
 import "./Footer.css";
+import { Link, useHistory } from "react-router-dom";
 
 function Home() {
+
+  const history = useHistory();
+
+  const routeToContact = () => {
+    history.push("/contact")
+  }
   return (
     <div className="home__main">
       <div className="home__banner">
@@ -21,7 +28,7 @@ function Home() {
             <p>I am a Full-Stack Developer</p>
           </div>
           <div className="home__button">
-            <button className="button__hire">Hire Me</button>
+            <button className="button__hire" onClick={routeToContact}>Hire Me</button>
             <a className="a__cv" href="./assets/cv.pdf" target="_blank">Download CV</a>
             
           </div>
